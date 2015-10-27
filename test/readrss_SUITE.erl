eacdd78472d250f318f47d4d148abbc6aed91b15
tundi <oawoyemi@alertlogic.com>
2015-10-27 20:48:57 +0000

    initial read rss test

--- test/readrss_SUITE.erl
+++ test/readrss_SUITE.erl
@@ -2,8 +2,15 @@
 -include_lib("common_test/include/ct.hrl").
 -export([all/0]).
 
--export([test1/1]).
+-export([test1/1, init_per_suite/1]).
 
 all() -> [test1].
 
-test1(_Config) -> 1 = 1.
+init_per_suite(Config) ->
+    inets:start(),
+    Config.
+
+readme() -> {ok, {_Status, _Headers, Body}}  = httpc:request(get, {"http://stackoverflow.com/feeds/tag?tagnames=erlang&sort=newest", []}, [], []),
+    Body.
+
+test1(_Config) -> ct:pal(" ~s ", [readme()]).
